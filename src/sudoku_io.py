10b3dcdb3da4b53e15e35b77c877305b89d7c632
BulouLiu <bulouliu@gmail.com>
2023-12-09 22:51:54 +0800

    test Ci

--- src/sudoku_io.py
+++ src/sudoku_io.py
@@ -12,8 +12,6 @@ def read_sudoku(filename):
 
 def print_sudoku(board):
     for i in range(9):
-        if i % 3 == 0 and i != 0:
-            print("---+---+---")
         for j in range(9):
             if j % 3 == 0 and j != 0:
                 print("|", end="")
